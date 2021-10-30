2d8e3340ff1a19883cebfae21131adb95397955c
ashen <jayasinghe.ashen@gmail.com>
2021-10-30 22:27:55 +0530

    py web3 build txn http ganache

--- deploy.py
+++ deploy.py
@@ -1,4 +1,11 @@
+from eth_utils import address
 from solcx import compile_standard, install_solc
+import json
+from web3 import Web3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 install_solc("0.6.0")
 
@@ -11,7 +18,7 @@ with open("./SimpleStorage.sol", "r") as file:
 compiled_sol = compile_standard(
     {
         "language": "Solidity",
-        "sources": {"simpleStorage.sol": {"content": simple_storage_file}},
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
         "settings": {
             "outputSelection": {
                 "*": {"*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]}
@@ -22,3 +29,46 @@ compiled_sol = compile_standard(
 )
 
 print(compiled_sol)
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+# deploy the contract
+
+# get bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+print(f"bytecode: {bytecode}")
+
+# get abi
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+print(f"abi: {abi}")
+
+# connect to ganache simulated /mock blockchain
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+chain_id = 1337
+my_address = "0xcAcd33e6B526Cc7906985B52222C527204C23ACC"
+# add 0x in python for private keys
+private_key = os.getenv("PRIVATE_KEY")
+
+# create the contract in python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+print(SimpleStorage)
+
+# send the transaction
+
+# get the latest transaction count
+nonce = w3.eth.getTransactionCount(my_address)
+print(nonce)
+# build the transaction
+transaction = SimpleStorage.constructor().buildTransaction(
+    {"chainId": chain_id, "from": my_address, "nonce": nonce}
+)
+print(transaction)
+
+# sign the transaction
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+
+# send the transaction
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
